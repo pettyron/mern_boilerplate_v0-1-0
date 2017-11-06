3260799b7cff1d23faaf3052b0c75f7d18046781
Ronnie Petty <rpetty.developer@gmail.com>
2017-11-06 10:01:55 -0600

    Changes to an environment based set up (handles production and development) This is set for Windows 10 but needs a more streamlined approach for consistency. Will eventually change to a Vagrant environment to keep things consistent.

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,51 @@ const path = require('path');
 const webpack = require('webpack');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 // TODO Configure Webpack for Development and Production Environments
+const dev = process.env.NODE_ENV === 'development';
+const prod = process.env.NODE_ENV === 'production';
+
+// console.log('Your environment is set to ' + process.env.NODE_ENV);
+
+function getPlugins() {
+    let plugins = [];
+
+    plugins.push(new webpack.DefinePlugin({
+        dev: JSON.stringify(dev),
+        prod: JSON.stringify(prod)
+    }));
+
+    plugins.push(new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.js',
+        minChunks: Infinity
+    }));
+
+    plugins.push(new ExtractTextPlugin('main.css'))
+
+    if (prod) {
+        plugins.push(
+            new UglifyJsPlugin()
+        );
+    } else {
+        plugins.push(
+            new BrowserSyncPlugin({
+                browser: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+                proxy: 'http://localhost:3000',
+                port: 7777,
+                reloadDelay: 600,
+                files: ['views/**/*.pug'],
+                notify: false
+            })
+        );
+    }
+    return plugins;
+}
+
 // TODO Configure Webpack with code splitting to deal with larger library sets i.e. React|Angular etc.
 
 const javascript = {
@@ -39,10 +80,12 @@ const styles = {
 
 const config = {
     entry: {
-        app: './public/scripts/app.js'
+        app: './public/scripts/app.js',
+        vendor: ['react', 'react-dom', 'react-router', 'gsap', 'hammerjs']
     },
     resolve: {
-        extensions: ['.js', 'jsx']
+        extensions: ['.js', 'jsx'],
+        modules: ['node_modules']
     },
     devtool: 'source-map',
     output: {
@@ -52,23 +95,10 @@ const config = {
     module: {
         rules: [javascript, styles]
     },
-    plugins: [
-        new BrowserSyncPlugin({
-            browser: 'chrome.exe',
-            proxy: 'http://localhost:3000',
-            port: 7777,
-            reloadDelay: 600,
-            files: ['views/**/*.pug'],
-            notify: false
-        }),
-        new ExtractTextPlugin('main.css'),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    ]
+    plugins: getPlugins()
 };
+
+console.log('Your environment is set to ' + process.env.NODE_ENV);
 
 process.noDeprecation = true;
 
